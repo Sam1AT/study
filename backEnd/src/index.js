5a79e2e8870e93461611019019bb64acb239e2dc
m0h0n <mahanbst85@gmal.com>
2021-02-01 14:56:00 -0500

    make hello, world

---
+++ backEnd/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express()
+
+const PORT = process.env.PORT || 8000
+
+app.use(express.json())
+
+app.get('/api/v1', (req, res) => {
+    res.json({Hello: "world"})
+})
+
+app.listen(PORT, err => {
+    if (err) {
+        return console.log(err)
+    }
+    console.log(`I'm running at port ${PORT}`)
+})
