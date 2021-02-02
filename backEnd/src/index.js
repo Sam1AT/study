6f8be9276420160fb35110d70eae236a64216d2c
m0h0n <mahanbst85@gmail.com>
2021-02-02 10:13:00 +0330

    make simple registry

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -1,13 +1,43 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+const User = require("./models/User");
+
+mongoose.connect(
+  "mongodb://localhost:27017/study",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("db is ok!");
+    }
+  }
+);
+
 app.use(express.json());
 
-app.get("/api/v1", (req, res) => {
-  res.json({ Hello: "world" });
+app.post("/api/v1/register", (req, res) => {
+  // Get post body
+  const body = req.body;
+  // make user in db
+  const user = new User({ username: body.username, password: body.password });
+  // save user in db and handle error and if ther is not error return some data
+  user.save((err) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      res.json({ usernaem: user.username });
+    }
+  });
 });
 
 app.listen(PORT, (err) => {
